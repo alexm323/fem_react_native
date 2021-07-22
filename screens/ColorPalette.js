ef1ecddf435cb39652e8b455b33ff519c31d1a16
alexm323 <alejandromartinezjr42@gmail.com>
2021-07-22 10:12:18 -0700

    navigation notes

---
+++ screens/ColorPalette.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import ColorBox from '../components/ColorBox';
+
+const ColorPalette = ({ route }) => {
+  const { colors, paletteName } = route.params;
+  return (
+    <FlatList
+      style={[styles.container]}
+      data={colors}
+      keyExtractor={(item) => item.hexCode}
+      renderItem={({ item }) => (
+        <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
+      )}
+      ListHeaderComponent={<Text style={styles.text}>{paletteName}</Text>}
+      ListFooterComponent={<View style={styles.footer} />}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 40,
+    paddingHorizontal: 10,
+    backgroundColor: 'white',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+  footer: {
+    height: 50,
+  },
+});
+
+export default ColorPalette;
